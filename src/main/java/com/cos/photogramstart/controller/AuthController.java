f8e108234fe3ce7d371508fcdb4245295a6170fb
DESKTOP-K9D48KS\mycom <dnwlsrla40@naver.com>
2022-04-14 01:03:01 +0900

    [feature] 회원가입 1차 완료 - AuthService, UserRepository, User 생성 - User Domain에 Dto -> Entity로 바꾸는 로직 추가 - Controller로 회원가입 요청을 Dto를 통해서 받고 그 정보를 Entity로 변경 해주는 로직 추가

--- src/main/java/com/cos/photogramstart/controller/AuthController.java
+++ src/main/java/com/cos/photogramstart/controller/AuthController.java
@@ -1,17 +1,28 @@
 package com.cos.photogramstart.controller;
 
+import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.dto.auth.SignupDto;
+import com.cos.photogramstart.service.AuthService;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
+@RequiredArgsConstructor    // final 필드 DI할 때 사용
 @Controller // 1.IOC등록 2.File 리턴하는 컨트롤러
 public class AuthController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private final AuthService authService;
+
+//    public AuthController(AuthService authService) {
+//        this.authService = authService;
+//    }
 
     @GetMapping("/auth/signin")
     public String signinForm() {
@@ -25,8 +36,9 @@ public class AuthController {
 
     @PostMapping("/auth/signup")
     public String signup(SignupDto signupDto) { // key=value (x-www-form-urlencoded) 기본적으로 Spring Controller가 받는 형식
-        logger.info(signupDto.toString());
+        User user = signupDto.toEntity();
+        User userEntity = authService.회원가입(user);
+        System.out.println("userEntity = " + userEntity);
         return "auth/signin";
     }
-
 }
