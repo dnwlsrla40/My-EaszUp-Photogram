a5cf2bcbf4e95ec13f237287ce8c8de2186772a0
김우진 <dnwlsrla40@naver.com>
2022-04-13 18:33:35 +0900

    [add] logback 사용 및 SignupDto 생성
    SignupDto 생성
     - username : 사용자 이름(Id)
     - password : 비밀번호
     - email : 이메일
     - name : 닉네임

--- src/main/java/com/cos/photogramstart/controller/AuthController.java
+++ src/main/java/com/cos/photogramstart/controller/AuthController.java
@@ -1,11 +1,17 @@
 package com.cos.photogramstart.controller;
 
+import com.cos.photogramstart.dto.auth.SignupDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+
 @Controller // 1.IOC등록 2.File 리턴하는 컨트롤러
 public class AuthController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/auth/signin")
     public String signinForm() {
@@ -18,9 +24,8 @@ public class AuthController {
     }
 
     @PostMapping("/auth/signup")
-    public String signup() {
-
-
+    public String signup(SignupDto signupDto) { // key=value (x-www-form-urlencoded) 기본적으로 Spring Controller가 받는 형식
+        logger.info(signupDto.toString());
         return "auth/signin";
     }
 
