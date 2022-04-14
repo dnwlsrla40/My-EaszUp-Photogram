6add9aa795d30ef1943e00b1f9550f2df25afd0a
김우진 <dnwlsrla40@naver.com>
2022-04-14 17:08:49 +0900

    [feature] 비밀번호 해시 - Bcrypt를 이용해서 비밀번호 해시 - SecurityConfig에 @Bean등록

--- src/main/java/com/cos/photogramstart/service/AuthService.java
+++ src/main/java/com/cos/photogramstart/service/AuthService.java
@@ -3,15 +3,23 @@ package com.cos.photogramstart.service;
 import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service // 1. IOC 관리 2. 트랜잭션 관리
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Transactional  // Write(Insert, Update, Delete) 일때 붙여주기
     public User 회원가입(User user) {
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        user.setRole("ROLE_USER");
         User userEntity = userRepository.save(user);
         return userEntity;
     }
