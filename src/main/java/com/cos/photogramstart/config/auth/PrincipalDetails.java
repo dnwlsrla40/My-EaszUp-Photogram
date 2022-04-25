aa6eb594bda7219522db779bb52f107f6be193de
DESKTOP-K9D48KS\mycom <dnwlsrla40@naver.com>
2022-04-25 18:44:52 +0900

    [feature] 로그인 기능을 위한 Custom UserDetails 생성 1. PrincipalDetailsService의 loadUserByUsername()의 반환형인 UserDetails에 맞추기 위해 Custom UserDetails인 PrincipalDetails 생성 2. PrincipalDetails의 구현에 필요한 함수들을 User Class를 이용해 구현 3. DB에 username으로 존재하는 User가 있는 지 확인하기 위해 JPA Query Method Creation으로 method 구현

---
+++ src/main/java/com/cos/photogramstart/config/auth/PrincipalDetails.java
@@ -0,0 +1,61 @@
+package com.cos.photogramstart.config.auth;
+
+import com.cos.photogramstart.domain.user.User;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Data
+public class PrincipalDetails implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    private User user;
+
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+    // 권한 : 한개가 아닐 수 있음(3개 이상의 권한)
+    // 따라서 GrantedAuthority Collection 방식으로 해야 하므로 하드 코딩으로 아래와 같은 방식으로 만듬
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collector = new ArrayList<>();
+        collector.add((GrantedAuthority) () -> user.getRole());
+        return collector;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
