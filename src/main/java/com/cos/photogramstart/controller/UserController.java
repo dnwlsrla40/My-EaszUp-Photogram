e356516da365ca3c524e57a29e5777ea1bdc765d
DESKTOP-K9D48KS\mycom <dnwlsrla40@naver.com>
2022-04-25 22:01:49 +0900

    [feature] Session을 다루기 위해 Security Session 찾기
    - 업데이트 시 필요한 login 정보를 가져오기 위해 Session 찾기
    - @AuthenticationPrincipal 이용

--- src/main/java/com/cos/photogramstart/controller/UserController.java
+++ src/main/java/com/cos/photogramstart/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.cos.photogramstart.controller;
 
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +15,9 @@ public class UserController {
     }
 
     @GetMapping("/user/{id}/update")
-    public String update(@PathVariable int id) {
+    public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        // SecurityContextHolder.getContext().getAuthentication().getPrincipal().getUser();
+        // 해줘야 할 것을 안하고 세션 정보를 가져올 수 있도록 Security에서 @AuthenticationPrincipal 제공
         return "user/update";
     }
 }
